760b3c5b7d543751f8115a14bde6ee7bbb1f13f1
Canarygo1 <33827581+Canarygo1@users.noreply.github.com>
2025-03-18 12:29:17 +0000

    Update: Extra categories for survey disabled

--- src/Data/obj/survey.ts
+++ src/Data/obj/survey.ts
@@ -1,10 +1,5 @@
 export const surveyOptions = [
   "Mejor-Guachinche-Moderno",
   "Mejor-Guachinche-Tradicional",
-  "Mejor-Restaurante-De-Pescado",
-  "Mejor-Restaurante",
-  "Mejor-Carne-Cabra",
-  "Mejor-Bocadillo",
-  "Mejor-Japones",
-  "Mejor-Vino",
+
 ];
